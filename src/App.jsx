34ebe52fa958bd4eb4cb981bc13269c41723bb67
Akshat-Dev-tech <akiiwork1234@gmail.com>
2026-06-02 00:21:57 +0530

    feat: add task input and display functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  
+  const [tasks, setTasks] = useState([])
+  const [task, setTask] = useState("")
+  const updateTask = () => {
+    setTasks([...tasks, task])
+    setTask('')
+  }
+
+  const renderToDos = (todo) => (
+      <li>{todo}</li>
+  )
 
   return (
     <>
-     <h1>Add your Tasks</h1>
+      <h1>Add your Tasks</h1>
+      <div>
+        <input type="text" placeholder='Enter your task' onChange={(e) => setTask(e.target.value)} value={task} />{" "}
+        <button type="button" onClick={updateTask}>Add Task</button>
+      </div>
+      <h2>Tasks List</h2>
+      <div >
+        <ul>
+          {tasks.length > 0 ?
+            tasks?.map((todo) => renderToDos(todo)
+            ) :
+            <p>No to do yet</p>
+          }
+        </ul>
+      </div>
     </>
   )
 }
