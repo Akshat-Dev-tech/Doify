8b967f1483d63c511f5ff9efcbe681d01ccb7b3c
Akshat-Dev-tech <akiiwork1234@gmail.com>
2026-06-04 20:32:06 +0530

    feat: mark completed tasks funcitonality

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,21 @@ function App() {
    setTasks(tasks.filter((_,index)=>index!=i))
   }
 
+  const completeTask=(e,todo ,i)=>{
+    setTasks(tasks.map((task,index)=>{
+      if(index===i){
+        let val = e.target.checked ? {...task,completed:true} : {...task,completed:false}
+        return val
+      }
+      else return task
+    }))
+  }
+
   const renderToDos = (todoobj,i) => (
     <div className='display-flex'>
-      <li>{todoobj.todo}</li>
-      <button type="button" onClick={()=>removeTask(todoobj.todo,i)}> Remove Tasks</button>
+      <li className={todoobj.completed ? 'completed' : ''}>{todoobj.todo}</li>
+      <button type="button" onClick={() => removeTask(todoobj.todo, i)}> Remove Tasks</button>
+      <input type="checkbox" onChange={(e)=>completeTask(e,todoobj.todo,i)}/>
     </div>
   )
 
